3341c9598833cd596d07172d5c859d2ea71e3509
Ivorysanna <violetta.pyralov@web.de>
2022-11-27 00:17:12 +0100

    Waking up Scene started

--- Template/Source/Scenes/wakingUp.ts
+++ Template/Source/Scenes/wakingUp.ts
@@ -1,22 +1,18 @@
 namespace Template {
     export async function wakingUp(): fS.SceneReturn {
-        console.log("First Scene starting");
+        console.log("Waking Up starting");
 
-        // let text = {
-        //     Ai: {
-        //         T0001: "Hallo",
-        //         T0002: "Anderer Text",
-        //         T0003: "Text Nummer 3",
-        //     },
-        // };
         fS.Speech.hide();
-        await fS.Location.show(location.uni);
-        await fS.Character.show(characters.Ai, characters.Ai.pose.neutral, fS.positions.bottomcenter);
-        //await ƒS.Character.show(characters.Ai, characters.Ai.pose.neutral, ƒS.positionPercent(60, 120));
+        await fS.Location.show(location.bedroom);
+        //await fS.Character.show(characters.TestChara, characters.TestChara.pose.neutral, fS.positions.bottomcenter);
+        //*** WECKER KLINGELN *** 
         await fS.update();
-        await fS.Speech.tell(characters.Ai, "Text in einer Zeile");
-        await fS.Speech.tell(characters.Ai, "Zweiter Text in einer Zeile");
+        await fS.Speech.tell(characters.RikasMutter, "Rika wach auf, sonst kommst du zu spät!");
+        await fS.Speech.tell(Rika, "Ja, ich bin schon wach.");
+        await fS.Speech.tell(characters.Rika, "<i>Ich sollte mich schnell anziehen, nicht dass Sagi wieder auf mich warten muss …</i>");
 
-
+        //*** AUSFADEN EINFADEN***
+        await fS.Character.show(characters.Rika, characters.TestChara.pose.neutral, fS.positions.bottomcenter);
+        //await fS.Speech.tell(Rika, )
     }
 }
