f1fa6ec47a7b77482d053d76c50a56bd40654a9a
Ivorysanna <violetta.pyralov@web.de>
2022-11-27 00:15:44 +0100

    Shortcuts added
    Added for easier Character use

---
+++ Template/Source/Shortcuts.ts
@@ -0,0 +1,13 @@
+namespace Template {
+    //*** RIKA *** 
+    export  let Rika = characters.Rika;
+    export  let RikaPose = characters.Rika.pose;
+
+    //*** SAGI ***
+    export  let Sagi = characters.Sagi;
+    export  let SagiPose = characters.Sagi.pose;
+
+    //*** Sho ***
+    export  let Sho = characters.Sho;
+    export  let ShoPose = characters.Sho.pose;
+}
